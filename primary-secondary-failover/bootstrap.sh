46f7560f0cc2021e4fab902ad1234d48bc2e284b
Alex Honor <alexhonor@yahoo.com>
2013-05-17 10:59:00 -0700

    merged changes from pull request

--- primary-secondary-failover/bootstrap.sh
+++ primary-secondary-failover/bootstrap.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
 
-#set -e 
-#set -u
+# Exit immediately on error or undefined variable.
+set -e 
+set -u
+
+# Process command line arguments.
 
 if [ $# -lt 2 ]
 then
@@ -16,7 +19,8 @@ BINTRAY_URL=$3
 # ----------------
 #
 # Utilities
-#
+# Bootstrap a fedora repo to get xmlstarlet
+
 curl -s http://dl.fedoraproject.org/pub/epel/6/x86_64/epel-release-6-8.noarch.rpm -o epel-release.rpm -z epel-release.rpm
 if ! rpm -q epel-release
 then
@@ -47,19 +51,18 @@ yum -y install rundeck
 # Reset the home directory permission as it comes group writeable.
 # This is needed for ssh requirements.
 chmod 755 ~rundeck
-
-# Configure the system
-#
-# Add vagrant user to rundeck group
+# Add vagrant user to rundeck group.
 usermod -g rundeck vagrant
+
 #
-# Disable the firewall so we can easily access it from the host
+# Disable the firewall so we can easily access it from any host.
 service iptables stop
 #
 
-# Configure rundeck
+# Configure rundeck.
 # -----------------
-# 
+#
+# Configure the mysql connection.
 cd /etc/rundeck
 cat >rundeck-config.properties.new <<EOF
 #loglevel.default is the default log level for jobs: ERROR,WARN,INFO,VERBOSE,DEBUG
@@ -74,6 +77,7 @@ EOF
 mv rundeck-config.properties.new rundeck-config.properties
 chown rundeck:rundeck rundeck-config.properties
 
+# Replace references to localhost with this node's name.
 sed "s/localhost/$NAME/g" framework.properties > framework.properties.new
 grep -q rundeck.server.uuid framework.properties.new || {
 UUID=$(uuidgen)
@@ -83,15 +87,20 @@ EOF
 }
 mv framework.properties.new framework.properties
 chown rundeck:rundeck framework.properties
-#
 
-# Set the rundeck password
-# 
+# Set the rundeck password. We need the password set
+# to allow us to interactively run ssh-copy-id.
 echo 'rundeck' | passwd --stdin rundeck
 
 # Start up rundeck
 # ----------------
-#
+
+# Check if rundeck is running and start it if necessary.
+# Checks if startup message is contained by log file.
+# Fails and exits non-zero if reaches max tries.
+
+set +e; # shouldn't have to turn off errexit.
+
 mkdir -p /var/log/vagrant
 if ! /etc/init.d/rundeckd status
 then
@@ -101,23 +110,25 @@ then
         /etc/init.d/rundeckd start 
     ) &> /var/log/rundeck/service.log # redirect stdout/err to a log.
 
-    let count=0
-    let max=18
+    success_msg="Started SocketConnector@"
+    let count=0 max=18
+
     while [ $count -le $max ]
     do
-        if ! grep  "Started SocketConnector@" /var/log/rundeck/service.log
-        then  printf >&2 ".";# progress output.
+        if ! grep "${success_msg}" /var/log/rundeck/service.log
+        then  printf >&2 ".";#  output message.
         else  break; # successful message.
         fi
-        let count=$count+1;# increment attempts
+        let count=$count+1;# increment attempts count.
         [ $count -eq $max ] && {
             echo >&2 "FAIL: Execeeded max attemps "
             exit 1
         }
-        sleep 10
+        sleep 10; # wait 10s before trying again.
     done
 fi
 
 echo "Rundeck started."
 
+# Done.
 exit $?
