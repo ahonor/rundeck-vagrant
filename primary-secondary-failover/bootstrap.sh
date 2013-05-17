06b93181971a651ca4a225b3ba4fa20a90db990c
Alex Honor <alexhonor@yahoo.com>
2013-05-17 09:55:00 -0700

    code cleanup

--- primary-secondary-failover/bootstrap.sh
+++ primary-secondary-failover/bootstrap.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
 
-#set -e 
-#set -u
+# Exit immediately on error or undefined variable.
+set -e 
+set -u
+
+# Process command line arguments.
 
 if [ $# -ne 2 ]
 then
@@ -10,12 +13,14 @@ then
 fi
 NAME=$1
 MYSQLADDR=$2
+RUNDECK_YUM_REPO=http://repo.rundeck.org/latest.rpm
 
 # Software install
 # ----------------
 #
 # Utilities
-#
+# Bootstrap a fedora repo to get xmlstarlet
+
 curl -s http://dl.fedoraproject.org/pub/epel/6/x86_64/epel-release-6-8.noarch.rpm -o epel-release.rpm -z epel-release.rpm
 if ! rpm -q epel-release
 then
@@ -31,26 +36,25 @@ yum -y install java-1.6.0
 #
 if ! rpm -q rundeck-repo
 then
-    rpm -Uvh http://repo.rundeck.org/latest.rpm 
+    rpm -Uvh "${RUNDECK_YUM_REPO}"
 fi
 yum -y install rundeck
 
 # Reset the home directory permission as it comes group writeable.
 # This is needed for ssh requirements.
 chmod 755 ~rundeck
-
-# Configure the system
-#
-# Add vagrant user to rundeck group
+# Add vagrant user to rundeck group.
 usermod -g rundeck vagrant
+
 #
-# Disable the firewall so we can easily access it from the host
+# Disable the firewall so we can easily access it from any host.
 service iptables stop
 #
 
-# Configure rundeck
+# Configure rundeck.
 # -----------------
-# 
+#
+# Configure the mysql connection.
 cd /etc/rundeck
 cat >rundeck-config.properties.new <<EOF
 #loglevel.default is the default log level for jobs: ERROR,WARN,INFO,VERBOSE,DEBUG
@@ -64,18 +68,24 @@ EOF
 mv rundeck-config.properties.new rundeck-config.properties
 chown rundeck:rundeck rundeck-config.properties
 
+# Replace references to localhost with this node's name.
 sed "s/localhost/$NAME/g" framework.properties > framework.properties.new
 mv framework.properties.new framework.properties
 chown rundeck:rundeck framework.properties
-#
 
-# Set the rundeck password
-# 
+# Set the rundeck password. We need the password set
+# to allow us to interactively run ssh-copy-id.
 echo 'rundeck' | passwd --stdin rundeck
 
 # Start up rundeck
 # ----------------
-#
+
+# Check if rundeck is running and start it if necessary.
+# Checks if startup message is contained by log file.
+# Fails and exits non-zero if reaches max tries.
+
+set +e; # shouldn't have to turn off errexit.
+
 mkdir -p /var/log/vagrant
 if ! /etc/init.d/rundeckd status
 then
@@ -85,23 +95,25 @@ then
         /etc/init.d/rundeckd start 
     ) &> /var/log/rundeck/service.log # redirect stdout/err to a log.
 
-    let count=0
-    let max=18
+    success_msg="Started SocketConnector@"
+    let count=0 max=18
+
     while [ $count -le $max ]
     do
-        if ! grep  "Started SocketConnector@" /var/log/rundeck/service.log
-        then  printf >&2 ".";# progress output.
+        if ! grep "${success_msg}" /var/log/rundeck/service.log
+        then  printf >&2 ".";#  output message.
         else  break; # successful message.
         fi
-        let count=$count+1;# increment attempts
+        let count=$count+1;# increment attempts count.
         [ $count -eq $max ] && {
             echo >&2 "FAIL: Execeeded max attemps "
             exit 1
         }
-        sleep 10
+        sleep 10; # wait 10s before trying again.
     done
 fi
 
 echo "Rundeck started."
 
+# Done.
 exit $?
--- primary-secondary-failover/install-mysql.sh
+++ primary-secondary-failover/install-mysql.sh
@@ -1,23 +1,32 @@
 #!/usr/bin/env bash
+
+# Exit immediately on error or undefined variable.
 set -e 
 set -u
 
 
 # 
-# Install the mysql server software
+# Install software.
+# -----------------
+
 #
-yum -y install mysql 
-yum -y install mysql-server 
-yum -y install mysql-devel 
+# Mysql.
+#
+yum -y install mysql  mysql-server mysql-devel 
 chgrp -R mysql /var/lib/mysql 
 chmod -R 770 /var/lib/mysql 
-#
+
 # Startup the server.
+# ------------------
 service mysqld start 
 
-sleep 20
+sleep 20; # let it bootup.
+
 #
-# Create the rundeck database.
+# Create the database.
+# --------------------
+
+# Create the rundeck database and grant access to any host.
 mysql --user root --password='' <<EOF
 create database rundeck;
 grant ALL on rundeck.* to 'rundeckuser'@'%' identified by 'rundeckpassword';
@@ -27,3 +36,6 @@ EOF
 
 # Shut off the firewall.
 service iptables stop
+
+# Done.
+exit $?
